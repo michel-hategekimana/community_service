1673d2d6dbbee6253691061331407c78edd21510
michel-hategekimana <michelhategekimana@127gmail.com>
2026-01-21 16:09:12 +0200

    finish all crud operation in categorycontroller and give acces to the provider tocreate category

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -1,6 +1,7 @@
 import Category from "../model/categoryModel.js"
 
 
+
 class CategoryController{
   static CreateCategory=async(req,res)=>{
         const category=await Category.create(req.body)
@@ -19,6 +20,53 @@ class CategoryController{
         }else{
             return res.status(200).json({message:"categories found successfully",categories})
         }
+    };
+
+    static getOneCategory=async(req,res)=>{
+        const {id}=req.params.id
+        const category=await Category.findById(id)
+        if(!category){
+            return res.status(404).json({message:"category not found"})
+
+        }else{
+            return res.status(200).json({message:"category found",category})
+        }
+
+    };
+
+    static deleteOneCategory=async(req,res)=>{
+        const {id}=req.params.id
+        const category=await Category.findByIdAndDelete(id)
+         if(!category){
+            return res.status(404).json({message:"category not found"})
+
+        }else{
+            return res.status(200).json({message:"category deleted",category})
+        }
+
+    };
+
+    static updateCategory=async(req,res)=>{
+        const id =req.params.id
+        const category =await Category.findByIdAndUpdate(id,req.body,{new:true})
+         if(!category){
+            return res.status(404).json({message:"category not found"})
+
+        }else{
+            return res.status(200).json({message:"category updated",category})
+        }
+
+    };
+
+    static deleteAllCategories=async(req,res)=>{
+        const categories=await Category.deleteMany()
+        if(!categories){
+            return res.status(404).json({message:"categories not found"})
+
+        }else{
+            return res.status(200).json({message:"categories deleted",categories})
+        }
+
     }
 
 
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,8 +1,14 @@
 import express from "express"
 import CategoryController from "../controller/categoryController.js"
+import { VerifyAcces } from "../midleware/verifyAccess.js"
+
 
 const router=express.Router()
-router.post("/create",CategoryController.CreateCategory)
+router.post("/create",VerifyAcces("provider"),CategoryController.CreateCategory)
 router.get("/categories",CategoryController.GetAllCategories)
+router.get("/:id",CategoryController.getOneCategory)
+router.delete("/:id",CategoryController.deleteOneCategory)
+router.patch("/:id",CategoryController.updateCategory)
+router.delete("/categories/delete",CategoryController.deleteAllCategories)
 
 export default router
